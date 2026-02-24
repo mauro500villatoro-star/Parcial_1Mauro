b055ae2657c6cd88e3cf99b512b8b615454e1180
Mauro Carrillo <mauro500villatoro@gmail.com>
2026-02-23 19:21:48 -0600

    Ya funcionando el ng serve

--- curso-tdap/src/app/components/login/login.component.ts
+++ curso-tdap/src/app/components/login/login.component.ts
@@ -1,19 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
   styleUrls: ['./login.component.scss'],
   template: `
-    <button (click)="login()">Login</button>
+    <h1>Login</h1>
+    <input placeholder="Usuario" #user>
+    <input type="password" placeholder="Password" #pass>
+    <button (click)="login(user.value, pass.value)">Login</button>
   `
 })
 export class LoginComponent {
-
   constructor(private authService: AuthService) {}
 
-  login() {
-    this.authService.login('admin', '1234')
-      .subscribe(res => console.log(res));
+  login(username: string, password: string) {
+    this.authService.login(username, password)
+      .subscribe((res: any) => {
+        if (res.success) {
+          console.log('Login correcto');
+        } else {
+          console.log('Credenciales inválidas');
+        }
+      });
   }
-
 }
